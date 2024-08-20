namnlm/workers-sdk

commit 0f6933917a942669cdaf1b2d752e67aa309811d7

MQ-605: Extend miniflare's queues simulator to support delayed delivery (#5570)
* MQ-605: Extend miniflare's queues simulator to support delayed delivery

This patch updates the miniflare's queue broker to support delayed
delivery of messages, both when sending the message from a producer and
when retrying the message from a consumer.
The implementation is based on miniflare's timers, and currently there
is no support for specifying the delivery delay in queue settings for a
producer.

* MQ-605: Add support for inheriting the delivery delay from the queues binding

* fix e2e tests

* chore: tighten turbo inputs to wrangler build to improve cache hits

---------

Co-authored-by: Peter Bacon Darwin <pbacondarwin@cloudflare.com>

packages/miniflare/src/index.ts
@@ -53,6 +53,7 @@ import {
 	ProxyClient,
 	QUEUES_PLUGIN_NAME,
 	QueueConsumers,
+	QueueProducers,
 	QueuesError,
 	R2_PLUGIN_NAME,
 	ReplaceWorkersTypes,
@@ -386,6 +387,28 @@ function getWrappedBindingNames(
 		}
 	}
 	return wrappedBindingWorkerNames;
+}
+
+function getQueueProducers(allWorkerOpts: PluginWorkerOptions[]): QueueProducers {
+	const queueProducers: QueueProducers = new Map();
+	for (const workerOpts of allWorkerOpts) {
+		const workerName = workerOpts.core.name ?? "";
+		let workerProducers = workerOpts.queues.queueProducers;
+
+		if (workerProducers !== undefined) {
+			// De-sugar array consumer options to record mapping to empty options
+			if (Array.isArray(workerProducers)) {
+				workerProducers = Object.fromEntries(
+					workerProducers.map((bindingName) => [bindingName, { queueName: bindingName }])
+				);
+			}
+
+			for (const [bindingName, opts] of Object.entries(workerProducers)) {
+				queueProducers.set(bindingName, { workerName, ...opts });
+			}
+		}
+	}
+	return queueProducers;
 }
 
 function getQueueConsumers(
@@ -1015,6 +1038,7 @@ export class Miniflare {
 			allWorkerOpts,
 			durableObjectClassNames
 		);
+		const queueProducers = getQueueProducers(allWorkerOpts);
 		const queueConsumers = getQueueConsumers(allWorkerOpts);
 		const allWorkerRoutes = getWorkerRoutes(allWorkerOpts, wrappedBindingNames);
 		const workerNames = [...allWorkerRoutes.keys()];
@@ -1137,6 +1161,7 @@ export class Miniflare {
 				wrappedBindingNames,
 				durableObjectClassNames,
 				unsafeEphemeralDurableObjects,
+				queueProducers,
 				queueConsumers,
 			};
 			for (const [key, plugin] of PLUGIN_ENTRIES) {

packages/miniflare/test/plugins/queues/index.spec.ts
@@ -205,11 +205,11 @@ test("sends all structured cloneable types", async (t) => {
 				path: "<script>",
 				contents: `
         import assert from "node:assert";
-        
+
         const arrayBuffer = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7]).buffer;
         const cyclic = { a: 1 };
         cyclic.b = cyclic;
-        
+
         const VALUES = {
           Object: { w: 1, x: 42n, y: true, z: "string" },
           Cyclic: cyclic,
@@ -247,7 +247,7 @@ test("sends all structured cloneable types", async (t) => {
             assert.deepStrictEqual(value.cause, VALUES.Error.cause, "Error: cause");
           }
         };
-        
+
         export default {
           async fetch(request, env, ctx) {
             await env.QUEUE.sendBatch(Object.entries(VALUES).map(
@@ -311,8 +311,7 @@ test("retries messages", async (t) => {
 	const log = new TestLog(t);
 	const mf = new Miniflare({
 		log,
-
-		queueProducers: { QUEUE: "queue" },
+		queueProducers: { QUEUE: { queueName: "queue" } },
 		queueConsumers: {
 			queue: { maxBatchSize: 5, maxBatchTimeout: 1, maxRetires: 2 },
 		},
@@ -560,7 +559,7 @@ test("moves to dead letter queue", async (t) => {
 		log,
 		verbose: true,
 
-		queueProducers: { BAD_QUEUE: "bad" },
+		queueProducers: { BAD_QUEUE: { queueName: "bad" } },
 		queueConsumers: {
 			// Check single Worker consuming multiple queues
 			bad: {
@@ -684,7 +683,7 @@ test("operations permit strange queue names", async (t) => {
 	const id = "my/ Queue";
 	const mf = new Miniflare({
 		verbose: true,
-		queueProducers: { QUEUE: id },
+		queueProducers: { QUEUE: { queueName: id } },
 		queueConsumers: [id],
 		serviceBindings: {
 			async REPORTER(request) {
@@ -732,7 +731,7 @@ test("supports message contentTypes", async (t) => {
 	const mf = new Miniflare({
 		log,
 		verbose: true,
-		queueProducers: { QUEUE: id },
+		queueProducers: { QUEUE: { queueName: id } },
 		queueConsumers: [id],
 		serviceBindings: {
 			async REPORTER(request) {
@@ -834,3 +833,142 @@ test("validates message size", async (t) => {
 			"Queue send failed: message length of 128001 bytes exceeds limit of 128000",
 	});
 });
+
+test("supports delivery delay", async (t) => {
+	let batches: string[][] = [];
+
+	const log = new TestLog(t);
+	const mf = new Miniflare({
+		log,
+		verbose: true,
+		queueProducers: { QUEUE: { queueName: "QUEUE", deliveryDelay: 2 } },
+		queueConsumers: {
+			QUEUE: { maxBatchSize: 100, maxBatchTimeout: 0, maxRetires: 1, retryDelay: 3 },
+		},
+		serviceBindings: {
+			async REPORTER(request) {
+				const batch = StringArraySchema.parse(await request.json())
+				if (batch.length > 0) {
+					batches.push(batch);
+				}
+				return new Response();
+			},
+		},
+		modules: true,
+		script: `export default {
+      async fetch(request, env, ctx) {
+		const delay = request.headers.get("X-Msg-Delay-Secs");
+		const url = new URL(request.url);
+		const body = await request.json();
+
+		if (url.pathname === "/send") {
+			if (delay === null) {
+				await env.QUEUE.send(body);
+			} else {
+				await env.QUEUE.send(body, { delaySeconds: Number(delay) });
+			}
+		} else if (url.pathname === "/batch") {
+		  await env.QUEUE.sendBatch(body, { delaySeconds: Number(delay) });
+		}
+        return new Response(null, { status: 204 });
+      },
+      async queue(batch, env, ctx) {
+        delete Date.prototype.toJSON; // JSON.stringify calls .toJSON before the replacer
+        await env.REPORTER.fetch("http://localhost", {
+          method: "POST",
+		  body: JSON.stringify(batch.messages.map(({ id }) => id)),
+        });
+
+		let isBatch = true;
+		for (const message of batch.messages) {
+			if (message.body < 10) {
+				if (message.body % 2 == 0)  {
+					message.retry({ delaySeconds: 20 });
+				}
+				isBatch = false;
+			}
+		}
+		batch.retryAll(isBatch ? {} : { delaySeconds: 10 });
+      },
+    };`,
+	});
+	t.teardown(() => mf.dispose());
+	const object = await getControlStub(mf, "QUEUE");
+
+	// Test send.
+
+	async function send(delay: number, message: unknown) {
+		await mf.dispatchFetch("http://localhost/send", {
+			method: "POST",
+			headers: { "X-Msg-Delay-Secs": delay.toString() },
+			body: JSON.stringify(message),
+		});
+	}
+
+	// Send 10 messages.
+	for (let i = 0; i < 10; i++) {
+		send(i, i);
+	}
+
+	// Verify messages are delivered at the right times.
+	for (let i = 1; i <= 10; i++) {
+		await object.advanceFakeTime(1000);
+		await object.waitForFakeTasks();
+		t.is(batches.length, i);
+	}
+
+	batches = [];
+
+	// Verify messages are retried at the right times.
+	for (let i = 1; i <= 10; i++) {
+		await object.advanceFakeTime(2000);
+		await object.waitForFakeTasks();
+		t.is(batches.length, i);
+	}
+
+	batches = [];
+
+	// Test send batch.
+
+	async function sendBatch(delay: number, ...messages: unknown[]) {
+		await mf.dispatchFetch("http://localhost/batch", {
+			method: "POST",
+			headers: { "X-Msg-Delay-Secs": delay.toString() },
+			body: JSON.stringify(messages),
+		});
+	}
+
+	// Send batch of messages (default batch delay: 1 sec).
+	sendBatch(1, { body: 10, delaySeconds: 0 }, { body: 11 }, { body: 12, delaySeconds: 2 })
+
+	// Verify messages are received at the right times.
+	for (let i = 1; i <= 3; i++) {
+		await object.advanceFakeTime(1000);
+		await object.waitForFakeTasks();
+		t.is(batches.length, i);
+	}
+
+	batches = [];
+
+	// Verify messages are retried at the right times.
+	for (let i = 1; i <= 3; i++) {
+		await object.advanceFakeTime(1000);
+		await object.waitForFakeTasks();
+		t.is(batches.length, i);
+	}
+
+	batches = [];
+
+	// Sending message with delivery delay set at the producer settings level.
+	await mf.dispatchFetch("http://localhost/send", {
+		method: "POST",
+		body: JSON.stringify("test"),
+	});
+	await object.advanceFakeTime(1000);
+	await object.waitForFakeTasks();
+	t.is(batches.length, 0);
+	await object.advanceFakeTime(1000);
+	await object.waitForFakeTasks();
+	t.is(batches.length, 1);
+});
+

packages/miniflare/src/plugins/queues/index.ts
@@ -9,6 +9,7 @@ import {
 import {
 	QueueBindings,
 	QueueConsumerOptionsSchema,
+	QueueProducerOptionsSchema,
 	SharedBindings,
 } from "../../workers";
 import { getUserServiceName } from "../core";
@@ -17,14 +18,12 @@ import {
 	SERVICE_LOOPBACK,
 	getMiniflareObjectBindings,
 	kProxyNodeBinding,
-	namespaceEntries,
-	namespaceKeys,
-	objectEntryWorker,
+	objectEntryWorker
 } from "../shared";
 
 export const QueuesOptionsSchema = z.object({
 	queueProducers: z
-		.union([z.record(z.string()), z.string().array()])
+		.union([z.record(QueueProducerOptionsSchema), z.string().array(), z.record(z.string())])
 		.optional(),
 	queueConsumers: z
 		.union([z.record(QueueConsumerOptionsSchema), z.string().array()])
@@ -42,23 +41,24 @@ const QUEUE_BROKER_OBJECT: Worker_Binding_DurableObjectNamespaceDesignator = {
 export const QUEUES_PLUGIN: Plugin<typeof QueuesOptionsSchema> = {
 	options: QueuesOptionsSchema,
 	getBindings(options) {
-		const queues = namespaceEntries(options.queueProducers);
+		const queues = bindingEntries(options.queueProducers);
 		return queues.map<Worker_Binding>(([name, id]) => ({
 			name,
 			queue: { name: `${SERVICE_QUEUE_PREFIX}:${id}` },
 		}));
 	},
 	getNodeBindings(options) {
-		const queues = namespaceKeys(options.queueProducers);
+		const queues = bindingKeys(options.queueProducers);
 		return Object.fromEntries(queues.map((name) => [name, kProxyNodeBinding]));
 	},
 	async getServices({
 		options,
 		workerNames,
+		queueProducers: allQueueProducers,
 		queueConsumers: allQueueConsumers,
 		unsafeStickyBlobs,
 	}) {
-		const queues = namespaceEntries(options.queueProducers);
+		const queues = bindingEntries(options.queueProducers);
 		if (queues.length === 0) return [];
 
 		const services = queues.map<Service>(([_, id]) => ({
@@ -97,6 +97,10 @@ export const QUEUES_PLUGIN: Plugin<typeof QueuesOptionsSchema> = {
 						},
 					},
 					{
+						name: QueueBindings.MAYBE_JSON_QUEUE_PRODUCERS,
+						json: JSON.stringify(Object.fromEntries(allQueueProducers)),
+					},
+					{
 						name: QueueBindings.MAYBE_JSON_QUEUE_CONSUMERS,
 						json: JSON.stringify(Object.fromEntries(allQueueConsumers)),
 					},
@@ -112,5 +116,29 @@ export const QUEUES_PLUGIN: Plugin<typeof QueuesOptionsSchema> = {
 		return services;
 	},
 };
+
+function bindingEntries(
+	namespaces?: Record<string, { queueName: string, deliveryDelay?: number }> | string[] | Record<string, string>
+): [bindingName: string, id: string][] {
+	if (Array.isArray(namespaces)) {
+		return namespaces.map((bindingName) => [bindingName, bindingName]);
+	} else if (namespaces !== undefined) {
+		return Object.entries(namespaces).map(([name, opts]) => [name, typeof opts === 'string' ? opts : opts.queueName]);
+	} else {
+		return [];
+	}
+}
+
+function bindingKeys(
+	namespaces?: Record<string, { queueName: string, deliveryDelay?: number }> | string[] | Record<string, string>
+): string[] {
+	if (Array.isArray(namespaces)) {
+		return namespaces;
+	} else if (namespaces !== undefined) {
+		return Object.keys(namespaces);
+	} else {
+		return [];
+	}
+}
 
 export * from "./errors";

packages/miniflare/src/plugins/shared/index.ts
@@ -16,7 +16,7 @@ import {
 	OptionalZodTypeOf,
 	PathSchema,
 } from "../../shared";
-import { Awaitable, QueueConsumerSchema, sanitisePath } from "../../workers";
+import { Awaitable, QueueConsumerSchema, QueueProducerSchema, sanitisePath } from "../../workers";
 import { UnsafeUniqueKey } from "./constants";
 
 export const DEFAULT_PERSIST_ROOT = ".mf";
@@ -45,6 +45,9 @@ export type DurableObjectClassNames = Map<
 	>
 >;
 
+// Maps queue names to producer worker options.
+export type QueueProducers = Map<string, z.infer<typeof QueueProducerSchema>>;
+
 // Maps queue names to the Worker that wishes to consume it. Note each queue
 // can only be consumed by one Worker, but one Worker may consume multiple
 // queues. Support for multiple consumers of a single queue is not planned
@@ -70,6 +73,7 @@ export interface PluginServicesOptions<
 	wrappedBindingNames: WrappedBindingNames;
 	durableObjectClassNames: DurableObjectClassNames;
 	unsafeEphemeralDurableObjects: boolean;
+	queueProducers: QueueProducers;
 	queueConsumers: QueueConsumers;
 }
 

packages/miniflare/src/workers/queues/broker.worker.ts
@@ -20,7 +20,11 @@ import {
 	QueueContentType,
 	QueueContentTypeSchema,
 	QueueIncomingMessage,
+	QueueMessageDelay,
+	QueueMessageDelaySchema,
 	QueueOutgoingMessage,
+	QueueProducer,
+	QueueProducersSchema,
 	QueuesBatchRequestSchema,
 	QueuesOutgoingBatchRequest,
 } from "./schemas";
@@ -63,6 +67,19 @@ function validateContentType(headers: Headers): QueueContentType {
 		throw new HttpError(
 			400,
 			`message content type ${format} is invalid; if specified, must be one of 'text', 'json', 'bytes', or 'v8'`
+		);
+	}
+	return result.data;
+}
+
+function validateMessageDelay(headers: Headers): QueueMessageDelay {
+	const format = headers.get("X-Msg-Delay-Secs");
+	if (!format) return undefined;
+	const result = QueueMessageDelaySchema.safeParse(Number(format));
+	if (!result.success) {
+		throw new HttpError(
+			400,
+			`message delay ${format} is invalid: ${result.error}`
 		);
 	}
 	return result.data;
@@ -168,6 +185,7 @@ interface PendingFlush {
 type QueueBrokerObjectEnv = MiniflareDurableObjectEnv & {
 	// Reference to own Durable Object namespace for sending to dead-letter queues
 	[SharedBindings.DURABLE_OBJECT_NAMESPACE_OBJECT]: DurableObjectNamespace;
+	[QueueBindings.MAYBE_JSON_QUEUE_PRODUCERS]?: unknown;
 	[QueueBindings.MAYBE_JSON_QUEUE_CONSUMERS]?: unknown;
 } & {
 	[K in `${typeof QueueBindings.SERVICE_WORKER_PREFIX}${string}`]:
@@ -176,15 +194,25 @@ type QueueBrokerObjectEnv = MiniflareDurableObjectEnv & {
 };
 
 export class QueueBrokerObject extends MiniflareDurableObject<QueueBrokerObjectEnv> {
+	readonly #producers: Record<string, QueueProducer | undefined>;
 	readonly #consumers: Record<string, QueueConsumer | undefined>;
 	readonly #messages: QueueMessage[] = [];
 	#pendingFlush?: PendingFlush;
 
 	constructor(state: DurableObjectState, env: QueueBrokerObjectEnv) {
 		super(state, env);
+
+		const maybeProducers = env[QueueBindings.MAYBE_JSON_QUEUE_PRODUCERS];
+		if (maybeProducers === undefined) this.#producers = {};
+		else this.#producers = QueueProducersSchema.parse(maybeProducers);
+
 		const maybeConsumers = env[QueueBindings.MAYBE_JSON_QUEUE_CONSUMERS];
 		if (maybeConsumers === undefined) this.#consumers = {};
 		else this.#consumers = QueueConsumersSchema.parse(maybeConsumers);
+	}
+
+	get #maybeProducer() {
+		return this.#producers[this.name];
 	}
 
 	get #maybeConsumer() {
@@ -235,13 +263,13 @@ export class QueueBrokerObject extends MiniflareDurableObject<QueueBrokerObjectE
 		// Get messages to retry. If dispatching the batch failed for any reason,
 		// retry all messages.
 		const retryAll = response.retryBatch.retry || response.outcome !== "ok";
-		const retryMessagesIds = new Set(response.retryMessages.map(msg => msg.msgId));
+		const retryMessages = new Map(response.retryMessages?.map((r) => [r.msgId, r.delaySeconds]))
+		const globalDelay = response.retryBatch.delaySeconds ?? consumer.retryDelay ?? 0;
 
 		let failedMessages = 0;
-		const toRetry: QueueMessage[] = [];
 		const toDeadLetterQueue: QueueMessage[] = [];
 		for (const message of batch) {
-			if (retryAll || retryMessagesIds.has(message.id)) {
+			if (retryAll || retryMessages.has(message.id)) {
 				failedMessages++;
 				const failedAttempts = message.incrementFailedAttempts();
 				if (failedAttempts < maxAttempts) {
@@ -249,7 +277,13 @@ export class QueueBrokerObject extends MiniflareDurableObject<QueueBrokerObjectE
 						LogLevel.DEBUG,
 						`Retrying message "${message.id}" on queue "${this.name}"...`
 					);
-					toRetry.push(message);
+
+					const fn = () => {
+						this.#messages.push(message);
+						this.#ensurePendingFlush();
+					};
+					const delay = retryMessages.get(message.id) ?? globalDelay;
+					this.timers.setTimeout(fn, delay * 1000);
 				} else if (consumer.deadLetterQueue !== undefined) {
 					await this.logWithLevel(
 						LogLevel.WARN,
@@ -270,9 +304,7 @@ export class QueueBrokerObject extends MiniflareDurableObject<QueueBrokerObjectE
 			formatQueueResponse(this.name, acked, batch.length, endTime - startTime)
 		);
 
-		// Add messages for retry back to the queue, and ensure we flush again if
-		// we still have messages
-		this.#messages.push(...toRetry);
+		// Ensure we flush again if we still have messages.
 		this.#pendingFlush = undefined;
 		if (this.#messages.length > 0) this.#ensurePendingFlush();
 
@@ -320,28 +352,39 @@ export class QueueBrokerObject extends MiniflareDurableObject<QueueBrokerObjectE
 		this.#pendingFlush = { immediate: delay === 0, timeout };
 	}
 
-	#enqueue(messages: QueueIncomingMessage[]) {
+	#enqueue(messages: QueueIncomingMessage[], globalDelay: number = 0) {
 		for (const message of messages) {
 			const randomness = crypto.getRandomValues(new Uint8Array(16));
 			const id = message.id ?? Buffer.from(randomness).toString("hex");
 			const timestamp = new Date(message.timestamp ?? this.timers.now());
 			const body = deserialise(message);
-			this.#messages.push(new QueueMessage(id, timestamp, body));
+			const msg = new QueueMessage(id, timestamp, body);
+
+			const fn = () => {
+				this.#messages.push(msg);
+				this.#ensurePendingFlush();
+			}
+
+			const delay = message.delaySecs ?? globalDelay;
+			this.timers.setTimeout(fn, delay * 1000);
 		}
-		this.#ensurePendingFlush();
 	}
 
 	@POST("/message")
 	message: RouteHandler = async (req) => {
 		validateMessageSize(req.headers);
 		const contentType = validateContentType(req.headers);
+		const delay = validateMessageDelay(req.headers);
 		const body = Buffer.from(await req.arrayBuffer());
 
 		// If we don't have a consumer, drop the message
 		const consumer = this.#maybeConsumer;
 		if (consumer === undefined) return new Response();
 
-		this.#enqueue([{ contentType, body }]);
+		this.#enqueue(
+			[{ contentType, delaySecs: delay, body }],
+			this.#maybeProducer?.deliveryDelay
+		);
 		return new Response();
 	};
 
@@ -352,13 +395,14 @@ export class QueueBrokerObject extends MiniflareDurableObject<QueueBrokerObjectE
 		// a no-op. This allows us to enqueue a maximum size batch with additional
 		// ID and timestamp information.
 		validateBatchSize(req.headers);
+		const delay = validateMessageDelay(req.headers) ?? this.#maybeProducer?.deliveryDelay;
 		const body = QueuesBatchRequestSchema.parse(await req.json());
 
 		// If we don't have a consumer, drop the message
 		const consumer = this.#maybeConsumer;
 		if (consumer === undefined) return new Response();
 
-		this.#enqueue(body.messages);
+		this.#enqueue(body.messages, delay);
 		return new Response();
 	};
 }

packages/miniflare/src/workers/queues/constants.ts
@@ -1,4 +1,5 @@
 export const QueueBindings = {
 	SERVICE_WORKER_PREFIX: "MINIFLARE_WORKER_",
+	MAYBE_JSON_QUEUE_PRODUCERS: "MINIFLARE_QUEUE_PRODUCERS",
 	MAYBE_JSON_QUEUE_CONSUMERS: "MINIFLARE_QUEUE_CONSUMERS",
 } as const;

packages/miniflare/src/workers/queues/schemas.ts
@@ -1,5 +1,21 @@
 import { Base64DataSchema, z } from "miniflare:zod";
 
+export const QueueMessageDelaySchema = z.number().int().min(0).max(43200).optional();
+
+export const QueueProducerOptionsSchema = /* @__PURE__ */ z.object({
+	// https://developers.cloudflare.com/queues/platform/configuration/#producer
+	queueName: z.string(),
+	deliveryDelay: QueueMessageDelaySchema,
+});
+
+export const QueueProducerSchema = /* @__PURE__ */ z.intersection(
+	QueueProducerOptionsSchema,
+	z.object({ workerName: z.string() })
+);
+export type QueueProducer = z.infer<typeof QueueProducerSchema>;
+export const QueueProducersSchema =
+	/* @__PURE__ */ z.record(QueueProducerSchema);
+
 export const QueueConsumerOptionsSchema = /* @__PURE__ */ z.object({
 	// https://developers.cloudflare.com/queues/platform/configuration/#consumer
 	// https://developers.cloudflare.com/queues/platform/limits/
@@ -7,6 +23,7 @@ export const QueueConsumerOptionsSchema = /* @__PURE__ */ z.object({
 	maxBatchTimeout: z.number().min(0).max(30).optional(), // seconds
 	maxRetires: z.number().min(0).max(100).optional(),
 	deadLetterQueue: z.ostring(),
+	retryDelay: QueueMessageDelaySchema,
 });
 export const QueueConsumerSchema = /* @__PURE__ */ z.intersection(
 	QueueConsumerOptionsSchema,
@@ -25,8 +42,11 @@ export const QueueContentTypeSchema = /* @__PURE__ */ z
 	.default("v8");
 export type QueueContentType = z.infer<typeof QueueContentTypeSchema>;
 
+export type QueueMessageDelay = z.infer<typeof QueueMessageDelaySchema>;
+
 export const QueueIncomingMessageSchema = /* @__PURE__ */ z.object({
 	contentType: QueueContentTypeSchema,
+	delaySecs: QueueMessageDelaySchema,
 	body: Base64DataSchema,
 	// When enqueuing messages on dead-letter queues, we want to reuse the same ID
 	// and timestamp

packages/wrangler/e2e/deployments.test.ts
@@ -84,7 +84,7 @@ describe("deployments", () => {
 		expect(normalize(stdout)).toMatchInlineSnapshot(`
 			"🚧\`wrangler deployments\` is a beta command. Please report any issues to https://github.com/cloudflare/workers-sdk/issues/new/choose
 			Deployment ID: 00000000-0000-0000-0000-000000000000
-			version ID: 00000000-0000-0000-0000-000000000000
+			Version ID:    00000000-0000-0000-0000-000000000000
 			Created on:    TIMESTAMP
 			Author:        person@example.com
 			Source:        Upload from Wrangler 🤠

commit e6528ff694bb029748931561bb3497b37674addb

fix(miniflare): Fix regression introduced in #5570

packages/miniflare/src/index.ts
@@ -409,8 +409,22 @@ function getQueueProducers(
 				);
 			}
 
-			for (const [bindingName, opts] of Object.entries(workerProducers)) {
-				queueProducers.set(bindingName, { workerName, ...opts });
+			type Entries<T> = { [K in keyof T]: [K, T[K]] }[keyof T][];
+			type ProducersIterable = Entries<typeof workerProducers>;
+			const producersIterable = Object.entries(
+				workerProducers
+			) as ProducersIterable;
+
+			for (const [bindingName, opts] of producersIterable) {
+				if (typeof opts === "string") {
+					// queueProducers: { "MY_QUEUE": "my-queue" }
+					queueProducers.set(bindingName, { workerName, queueName: opts });
+				} else {
+					opts;
+					// queueProducers: ["MY_QUEUE"] or
+					// queueProducers: { QUEUE: { queueName: "QUEUE", ... } }
+					queueProducers.set(bindingName, { workerName, ...opts });
+				}
 			}
 		}
 	}

packages/miniflare/test/plugins/queues/index.spec.ts
@@ -804,7 +804,7 @@ test("supports message contentTypes", async (t) => {
 test("validates message size", async (t) => {
 	const mf = new Miniflare({
 		verbose: true,
-		queueProducers: ["QUEUE"],
+		queueProducers: { QUEUE: "my-queue" },
 		modules: true,
 		script: `export default {
       async fetch(request, env, ctx) {